d70ba09d007be981b5fc382c26c8e43bdaf73f61
BrayanArango11 <barango@uniempresarial.edu.co>
2023-08-27 09:56:00 -0500

    Events With JS
    New images with events when we click on it, change to another image or open a little pop up, the main pages i think that are ok :D

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,26 @@
 'use strict'
+import {showPasswordFunction, showContainerPasswordAndHideEmail, showPasswordFunction2, 
+        showContainerOptionsNav} from './animations.js';
+
+const btnHiddenPassword = document.getElementById('hiddenPassword'); 
+const btnShowPassword = document.getElementById('showPassword');
+const btnHiddenPassword2 = document.getElementById('hiddenPassword2'); 
+const btnShowPassword2 = document.getElementById('showPassword2');
+
+const btnChangeEmailToPassword = document.getElementById('buttonChangeContainer');
+const btnReturnEmailToPassword = document.getElementById('buttonReturnToEmail');
+
+const btnImage = document.getElementById('arrowImage');
+
+btnShowPassword?.addEventListener('click', showPasswordFunction);
+btnHiddenPassword?.addEventListener('click', showPasswordFunction);
+btnShowPassword2?.addEventListener('click', showPasswordFunction2);
+btnHiddenPassword2?.addEventListener('click', showPasswordFunction2);
+
+
+btnChangeEmailToPassword?.addEventListener('click', showContainerPasswordAndHideEmail);
+btnReturnEmailToPassword?.addEventListener('click', showContainerPasswordAndHideEmail);
+
+btnImage?.addEventListener('click', showContainerOptionsNav);
+
+
